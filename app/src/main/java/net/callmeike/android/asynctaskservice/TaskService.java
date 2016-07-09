520b6de262704aebaae3690573ffc4f65b982a81
Blake Meike <blake@cecilia.callmeike.net>
2016-07-09 10:02:40 -0700

    Update tooling

--- app/src/main/java/net/callmeike/android/asynctaskservice/TaskService.java
+++ app/src/main/java/net/callmeike/android/asynctaskservice/TaskService.java
@@ -1,5 +1,5 @@
 /* $Id: $
-   Copyright 2012, G. Blake Meike
+   Copyright 2015, 2016 G. Blake Meike
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
